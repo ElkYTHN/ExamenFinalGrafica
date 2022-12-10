dc9bffa8e3c035086e0492efe4af427e3815d31b
ElkYTHN <elkinportillo4@gmail.com>
2022-12-09 20:23:11 -0600

    Primero Cambios

---
+++ ProyectoFinal_Monica_Elkin/Library/Bee/Android/Prj/Mono2x/Gradle/launcher/build/generated/source/buildConfig/release/com/uth/ProyectoFinal_Monica_Elkin/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.uth.ProyectoFinal_Monica_Elkin;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.uth.ProyectoFinal_Monica_Elkin";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "0.1";
+}
